7e8b507cd6c5470557a7b0c39309d6f5013879e5
gastoncarriquiry <tato.carri@gmail.com>
2022-11-05 21:22:28 -0300

    menu styles and functionality

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,19 @@
 import { forwardRef } from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toggleMenuState } from "../../features/menuSlice";
+import Button from "../Button/Button";
 import "./Menu.css";
 
 const Menu = forwardRef((props, ref) => {
   const menuState = useSelector((state) => state.menu.menuState);
   const menu = useRef(null);
+  const [isVisible, setIsVisible] = useState(true);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleOutsideClick = (evt) => {
@@ -29,15 +33,35 @@ const Menu = forwardRef((props, ref) => {
     //eslint-disable-next-line
   }, [ref]);
 
+  const toggleModal = () => setIsVisible(!isVisible);
+  const handleClick = () => dispatch(toggleMenuState(false));
+  const deleteData = () => {
+    dispatch(toggleMenuState(false));
+    localStorage.clear();
+    setIsVisible(false);
+    navigate("/");
+  };
+
   return (
     <div ref={menu} className={`menu-list ${menuState ? "deployed" : "retracted"}`}>
       <ul>
-        <li>Ir a inicio</li>
-        <li>
-          <Link to="/descartados">Mostrar descartados</Link>
+        <li onClick={handleClick}>
+          <Link to="/">Ir a inicio</Link>
         </li>
-        <li>Eliminar datos</li>
+        <li onClick={handleClick}>
+          <Link to="/descartados">Lista de descartados</Link>
+        </li>
+        <li onClick={toggleModal}>Eliminar datos</li>
       </ul>
+      <div className={`modal-container ${isVisible ? "visible" : "hidden"}`}>
+        <div className="modal">
+          <h2>Usted está a un paso de eliminar todos los datos almacenados. ¿Desea continuar?</h2>
+          <div className="buttons">
+            <button onClick={toggleModal}>Cancelar</button>
+            <Button text="confirmar" type="primary" onClick={deleteData} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 });

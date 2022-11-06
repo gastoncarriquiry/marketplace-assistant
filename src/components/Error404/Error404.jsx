c43d6975b9058e8da9b5c59e22177aaf7b4c0edd
gastoncarriquiry <tato.carri@gmail.com>
2022-11-06 13:06:43 -0300

    Error404 UI for non-existent urls

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,8 +1,30 @@
+import { Link } from "react-router-dom";
 import "./Error404.css";
 
 const Error404 = () => {
-  //TODO: error 404
-  return <div>Error404</div>;
+  return (
+    <section className="error404">
+      <h1>Error 404</h1>
+      <h2>Lo sentimos. No encontramos lo que está buscando.</h2>
+      <div className="useful-links">
+        <p>Links útiles:</p>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/resultados">Resultados de búsqueda</Link>
+          </li>
+          <li>
+            <Link to="/favoritos">Lista de favoritos</Link>
+          </li>
+          <li>
+            <Link to="/descartados">Lista de descartados</Link>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
